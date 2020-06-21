58b204151e42feb1cd252447557d4927cd8ee4b6
Simon Mihevc <simon.mihevc@gmail.com>
2020-06-21 23:42:46 +0200

    Add basic navigation image and commands

--- src/app/navigate/navigate.page.ts
+++ src/app/navigate/navigate.page.ts
@@ -10,6 +10,10 @@ const iconUrl = "assets/marker-icon.png";
 const shadowUrl = "assets/marker-shadow.png";
 
 const car1 = "assets/img/parking/car1.png";
+const forward = "assets/img/navigation/gps-forward.png";
+const left = "assets/img/navigation/gps-left.png";
+const right = "assets/img/navigation/gps-right.png";
+const spot = "assets/img/navigation/gps-parking.png";
 
 const yx = Leaflet.latLng;
 const iconDefault = Leaflet.icon({
@@ -23,22 +27,15 @@ const iconDefault = Leaflet.icon({
   shadowSize: [41, 41],
 });
 
-const xy = (x, y) => {
-  if (Leaflet.Util.isArray(x)) {
-    // When doing xy([x, y]);
-    return yx(x[1], x[0]);
-  }
-  return yx(y, x); // When doing xy(x, y);
-};
-
 @Component({
   selector: "page-navigate",
   templateUrl: "navigate.page.html",
   styleUrls: ["./navigate.page.scss"],
 })
 export class NavigatePage {
-  map: Leaflet.Map;
+  public imageName = forward;
   public showTypes = true;
+  public directions = "Calculating path to free spot";
   isMap = true;
   constructor(private http: HttpClient) {
   }
@@ -52,7 +49,6 @@ export class NavigatePage {
 	*/
   }
 
-
   showForm() {
     this.showTypes = true;
   }
@@ -61,6 +57,5 @@ export class NavigatePage {
   }
 
   ionViewWillLeave() {
-    this.map.remove();
   }
 }
