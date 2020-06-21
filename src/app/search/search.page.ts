9c46a0cfde7090c393f3768c6ac0b68097a26bf0
Cherchercher <ib.gaga@hotmail.com>
2020-06-21 00:51:02 -0700

    add custom leaflet icons

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-import { ParkingService } from '../services/parking.service';
-import '../../../node_modules/leaflet/dist/leaflet.css';
-import * as Leaflet from 'leaflet';
-const iconRetinaUrl = 'assets/marker-icon-2x.png';
-const iconUrl = 'assets/marker-icon.png';
-const shadowUrl = 'assets/marker-shadow.png';
+import { Component } from "@angular/core";
+import { ParkingService } from "../services/parking.service";
+import "../../../node_modules/leaflet/dist/leaflet.css";
+import * as Leaflet from "leaflet";
+const car1 = "assets/img/parking/car1.png";
+const car2 = "assets/img/parking/car2.png";
+const car3 = "assets/img/parking/car3.png";
+const parkingSign = "assets/img/parking/parking.png";
+const iconRetinaUrl = "assets/marker-icon-2x.png";
+const iconUrl = "assets/marker-icon.png";
+const shadowUrl = "assets/marker-shadow.png";
+const yx = Leaflet.latLng;
 const iconDefault = Leaflet.icon({
   iconRetinaUrl,
   iconUrl,
@@ -16,10 +21,18 @@ const iconDefault = Leaflet.icon({
   shadowSize: [41, 41],
 });
 
+const xy = (x, y) => {
+  if (Leaflet.Util.isArray(x)) {
+    // When doing xy([x, y]);
+    return yx(x[1], x[0]);
+  }
+  return yx(y, x); // When doing xy(x, y);
+};
+
 @Component({
-  selector: 'page-search',
-  templateUrl: 'search.page.html',
-  styleUrls: ['./search.page.scss'],
+  selector: "page-search",
+  templateUrl: "search.page.html",
+  styleUrls: ["./search.page.scss"],
 })
 export class SearchPage {
   map: Leaflet.Map;
@@ -31,16 +44,18 @@ export class SearchPage {
   }
 
   ionViewDidEnter() {
-    this.map = new Leaflet.Map('mapParking').setView([33.8003, -117.8827], 16);
+    this.map = Leaflet.map("mapParking", {
+      crs: Leaflet.CRS.Simple,
+      minZoom: -5,
+    });
 
-    Leaflet.tileLayer(
-      'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
-      {
-        attribution: 'goplanatrip.com',
-      }
-    ).addTo(this.map);
+    const bounds = [
+      [0, 0],
+      [1000, 1000],
+    ];
 
-    this.setMarkers();
+    this.map.fitBounds(bounds);
+    this.setFakeMarkers();
   }
 
   setMarkers() {
@@ -53,6 +68,64 @@ export class SearchPage {
     }
   }
 
+  setFakeMarkers() {
+    Leaflet.Marker.prototype.options.icon = iconDefault;
+    const sol = xy(175, 145);
+    const mizar = xy(41.6, 130.1);
+    const kruegerZ = xy(13.4, 56.5);
+    const deneb = xy(218, 8);
+    const sol2 = xy(275, 145);
+
+    Leaflet.marker(sol, {
+      icon: Leaflet.icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: car1,
+      }),
+    })
+      .addTo(this.map)
+      .bindPopup("Sol");
+    Leaflet.marker(mizar, {
+      icon: Leaflet.icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: car2,
+      }),
+    })
+      .addTo(this.map)
+      .bindPopup("Mizar");
+    Leaflet.marker(kruegerZ, {
+      icon: Leaflet.icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: car3,
+      }),
+    })
+      .addTo(this.map)
+      .bindPopup("Krueger-Z");
+    Leaflet.marker(deneb, {
+      icon: Leaflet.icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: parkingSign,
+      }),
+    })
+      .addTo(this.map)
+      .bindPopup("Deneb");
+
+    Leaflet.marker(sol2, {
+      icon: Leaflet.icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: car1,
+      }),
+    })
+      .addTo(this.map)
+      .bindPopup("Sol 2");
+
+    Leaflet.polyline([sol, deneb]).addTo(this.map);
+  }
+
   showForm() {
     this.showTypes = true;
   }
@@ -61,7 +134,7 @@ export class SearchPage {
     if (this.isMap === true) {
       {
         // For each layer on the map remove everything and clear variables
-        this.map.eachLayer(function(layer) {
+        this.map.eachLayer(function (layer) {
           this.map.remove();
           this.map.removeLayer(layer);
           Leaflet.PointManager.deletePoints(); // clear the pointManager array of points
